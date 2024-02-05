b9f12da94d98a412be99236579257dc4c5ea7a21
Kaan Karahanlı <136744983+vante-dev@users.noreply.github.com>
2024-02-05 15:41:15 +0100

    Create systems.js

---
+++ systems.js
@@ -0,0 +1,39 @@
+import { ActivityType, Application } from 'discord.js';
+
+// Doldurmanız gereken tek şeyler bunlardır.
+
+const system = {
+    serverID: '',
+    mongoURL: '',
+    owners: [],
+    prefix: [],
+
+    Presence: {
+        Status: 'online',
+        Type: ActivityType.Playing,
+        Message: [],
+    },
+
+    Moderation: {
+        Main: '',
+        Stats: '',
+        Controller: '',
+    },
+
+    Security: {
+        Watcher: '',
+        Backuper: '',
+        Protector: '',
+        vanityAuth: '',
+        ownerAuth: '',
+        
+        Backups: [],
+    },
+
+    Welcome: {
+        Tokens: [],
+        Channel: [],
+    },
+}
+
+export default system;
